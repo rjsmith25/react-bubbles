a86ff2f9e312c5241a8ad2bdd3d6337a7c7f9fd3
Russell Smith <russellsmith@MacBook-Pro.local>
2020-05-15 20:36:37 -0400

    completed sprint mvp plus challenge

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import PrivateRoute from "./components/privateroute";
+import BubblePage from "./components/BubblePage";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -7,12 +9,25 @@ import "./styles.scss";
 function App() {
   return (
     <Router>
+      <header>
+        <div className="container">
+          <Link className="brand-name" to="/">
+            Bubbles
+          </Link>
+          <ul className="nav-list">
+            <li>
+              <Link className="nav-link" to="/bubble-page">
+                bubble page
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </header>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <PrivateRoute path="/bubble-page" component={BubblePage} />
+        </Switch>
       </div>
     </Router>
   );
